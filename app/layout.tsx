0b0aa1dfc1d073d84bcc97fb092fec281feb2384
Võ Hoàng Phúc <contact@vohoangphuc.com>
2026-03-21 00:46:20 +0700

    feat: integrate Vercel Analytics for enhanced tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,56 +1,57 @@
+import { Analytics } from "@vercel/analytics/next";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-
 import "./globals.css";
 
 const monogram = localFont({
-  src: [
-    {
-      path: "../public/fonts/monogram/ttf/monogram.ttf",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "../public/fonts/monogram/ttf/monogram-extended.ttf",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "../public/fonts/monogram/ttf/monogram-extended-italic.ttf",
-      weight: "400",
-      style: "italic",
-    },
-  ],
-  variable: "--font-varela",
-  display: "swap",
-  fallback: ["monospace"],
-  adjustFontFallback: false,
-  declarations: [
-    {
-      prop: "size-adjust",
-      value: "150%",
-    },
-  ],
+	src: [
+		{
+			path: "../public/fonts/monogram/ttf/monogram.ttf",
+			weight: "400",
+			style: "normal",
+		},
+		{
+			path: "../public/fonts/monogram/ttf/monogram-extended.ttf",
+			weight: "400",
+			style: "normal",
+		},
+		{
+			path: "../public/fonts/monogram/ttf/monogram-extended-italic.ttf",
+			weight: "400",
+			style: "italic",
+		},
+	],
+	variable: "--font-varela",
+	display: "swap",
+	fallback: ["monospace"],
+	adjustFontFallback: false,
+	declarations: [
+		{
+			prop: "size-adjust",
+			value: "150%",
+		},
+	],
 });
 
 export const metadata: Metadata = {
-  title: "Junly's Portfolio",
-  description: "A Nintendo Wii U inspired portfolio website for Junly.",
+	title: "Junly's Portfolio",
+	description: "A Nintendo Wii U inspired portfolio website for Junly.",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" className={monogram.variable}>
-      <body
-        className="font-sans text-[21px] md:text-[22px] antialiased overflow-hidden bg-[#e6f3ff]"
-        suppressHydrationWarning
-      >
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en" className={monogram.variable}>
+			<body
+				className="font-sans text-[21px] md:text-[22px] antialiased overflow-hidden bg-[#e6f3ff]"
+				suppressHydrationWarning
+			>
+				<Analytics />
+				{children}
+			</body>
+		</html>
+	);
 }
