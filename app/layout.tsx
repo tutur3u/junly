9aa9f8255fa752bed17ef1e0f0f41971d1bed6c8
Võ Hoàng Phúc <contact@vohoangphuc.com>
2026-03-25 00:53:46 +0700

    Revert "chore: switch to clarity font"
    This reverts commit 8bc03e1388c9426c08e3ccc85091df5b4b7202c6.

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,14 +4,34 @@ import { NuqsAdapter } from "nuqs/adapters/next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const clarity = localFont({
-	src: "../public/fonts/clarity/clarity.ttf",
-	weight: "400",
-	style: "normal",
+const monogram = localFont({
+	src: [
+		{
+			path: "../public/fonts/monogram/ttf/monogram.ttf",
+			weight: "400",
+			style: "normal",
+		},
+		{
+			path: "../public/fonts/monogram/ttf/monogram-extended.ttf",
+			weight: "400",
+			style: "normal",
+		},
+		{
+			path: "../public/fonts/monogram/ttf/monogram-extended-italic.ttf",
+			weight: "400",
+			style: "italic",
+		},
+	],
 	variable: "--font-varela",
 	display: "swap",
-	fallback: ["ui-sans-serif", "system-ui", "sans-serif"],
+	fallback: ["monospace"],
 	adjustFontFallback: false,
+	declarations: [
+		{
+			prop: "size-adjust",
+			value: "150%",
+		},
+	],
 });
 
 export const metadata: Metadata = {
@@ -25,7 +45,7 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en" className={clarity.variable}>
+		<html lang="en" className={monogram.variable}>
 			<body
 				className="font-sans text-[21px] md:text-[22px] antialiased overflow-hidden bg-[#e6f3ff]"
 				suppressHydrationWarning
